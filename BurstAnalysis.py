3095c6f0548934f5d00d360d2ed646d3be8971d1
samfdhanani <165212066+samfdhanani@users.noreply.github.com>
2024-08-07 21:47:01 -0400

    Update BurstAnalysis.py

--- BurstAnalysis.py
+++ BurstAnalysis.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 filelist = []
 pathslist = []
 
-datapath = os.path.normpath("/users/samdhanani/Desktop/MuhleLab/Operant_Data_Folders/CohF_DRH")
-IDList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+datapath = os.path.normpath("/users/samdhanani/Desktop/MuhleLab/Operant_Data_Folders/CohortFolder")
+IDList = [1, 2, 3, 4]
 
 for subdir, dirs, files in sorted(os.walk(datapath)):
     filelist.append(files)
@@ -133,16 +133,7 @@ def data_construct(data):
     LP_noReward = LP_noReward.tolist()
     x = 0
 
-    # looking when press starts
-    # find duration, subtraction--go through array with inex
-    # presses [i]-presses [i+1]
-    #compare presses I timestamp
-    # if timestamp is in the dipper, you can include the 5 sec in calculation
-    #p1 and 2 in for loop
-
-    #u = np.unique(events)
     LA = []
-
 
     for i in range (0, len(LeverPress)):
         p1 = np.divide(LeverPress[i], 10000000)
@@ -164,13 +155,12 @@ def data_construct(data):
 
     LA_array = np.array(LA)
    
-
     bursts = []
     burst_group = []
 
     for i in range(len(LA_array)):
     # Check if current value is between 1-2s or less than 1s
-        if LA_array[i] >= 2 and LA_array[i] <= 3:         ######CHANGE#######################################################################################S 
+        if LA_array[i] >= 2 and LA_array[i] <= 3:         ######CHANGE 
             burst_group.append(LA_array[i])
         else:
             if len(burst_group) >= 2:  # ensures that the burst is 2 or more in a row
@@ -202,42 +192,34 @@ def data_construct(data):
     LA_array = LA_array.replace('\n', ' ')
     LA_array = ' '.join(LA_array.split())
 
-    
     average = average_LA(LA)
     print("The average of LA is:", average)
     print('Mouse - ', Full_ID)
     print("Rewards - ", len(Reward))
     print("Headpoke - ", no_headpoke_count)
 
-
     return(latency, average, Sess_time, len(Reward), no_headpoke_count,len(LeverPress), Reward_rate, LP_noReward, LP_rate, LA_array, plot, bursts, len(bursts), burst_sums, avg_allburst, sum_allburst, average_burst_press_count)
  
-
 def genotype(sub):
-    # 1 = WT and 2 = Het
     g_type = None
-    if sub == 4 or sub == 5 or sub == 7 or sub == 8 or sub == 9 or sub == 11 or sub == 12 or sub == 13 or sub == 17 or sub == 19 or sub == 21 or sub == 22 or sub == 25 or sub == 28 or sub == 29 or sub == 31:
-        g_type = 1
-    elif sub == 1 or sub == 2 or sub == 3 or sub == 6 or sub == 10 or sub == 14 or sub == 15 or sub == 16 or sub == 18 or sub == 20 or sub == 23 or sub == 24 or sub == 26 or sub == 27 or sub == 30 or sub == 32:
-        g_type = 2
+    if sub == 1 or sub == 2:
+        g_type = 'Control'
+    elif sub == 3 or sub == 4:
+        g_type = 'Experimental'
 
     return g_type
 
 def sex(sub):
     s_type = None
-    if sub == 1 or sub == 2 or sub == 3 or sub == 4 or sub == 5 or sub == 6 or sub == 7 or sub == 8 or sub == 9 or sub == 10 or sub == 11 or sub == 12 or sub == 13 or sub == 14 or sub == 15 or sub == 16:
-        s_type = 'M'
-    elif sub == 17 or sub == 18 or sub == 19 or sub == 20 or sub == 21 or sub == 22 or sub == 23 or sub == 24 or sub == 25 or sub == 26 or sub == 27 or sub == 28 or sub == 29 or sub == 30 or sub == 31 or sub == 32:
-        s_type = 'F'
+    if sub == 1 or sub == 3:
+        s_type = 'Male'
+    elif sub == 2 or sub == 4:
+        s_type = 'Female'
 
     return s_type
 
-# pathslist = query(pathslist, '7-20-22')
 df_ind = 0 #index varibale, add one everytime we run through a subj
-sus_attn_df = pd.DataFrame(columns = ['Date', 'Subject', 'Genotype', 'Sex', 'FirstLatency', 'AverageLatency', 'SessionTime', 'Program', 'NumberOfRewards', 'no headpoke count','Lever Press', 'Reward Rate', 'lever press no reward', 'Rate (LP/SessTime)', 'all latency','plot','bursts','number of bursts', 'burst sums','average burst time', 'sum of all bursts', 'average_burst_press_count'])
-def new_func(session_type, ID, progline):
-    sess_type = session_type(progline, ID)
-    return sess_type
+assay_df = pd.DataFrame(columns = ['Date', 'Subject', 'Genotype', 'Sex', 'FirstLatency', 'AverageLatency', 'SessionTime', 'Program', 'NumberOfRewards', 'no headpoke count','Lever Press', 'Reward Rate', 'lever press no reward', 'Rate (LP/SessTime)', 'all latency','plot','bursts','number of bursts', 'burst sums','average burst time', 'sum of all bursts', 'average_burst_press_count'])
 def new_func(g_type, ID):
     g_type = genotype(ID)
     return g_type
@@ -248,9 +230,7 @@ def new_func(s_type, ID):
 for dirs in pathslist:
 
     date = os.path.basename(os.path.normpath(dirs))
-    # sus_attn_df = pd.DataFrame(columns = ['Date', 'Subject', 'PercentChoiceTrials', 'PercentChoiceCorrect', 'AverageLatency', 'TimedOutTrials', 'SessionTime', 'SessionType'])
     csv_name = "\\" + date + ".csv"
-    # print(date)
     
     for ID in IDList: #run througb ID list one by one, add subj to this to prevent confusion
         Full_ID = "Subject " + str(ID)
@@ -261,10 +241,7 @@ for dirs in pathslist:
         g_type = genotype(ID)
         s_type = sex(ID)
 
-
-        sus_attn_df.loc[df_ind] = [date, Full_ID, g_type, s_type, latency, num_average, Sess_time, progline, num_Rewards, no_headpoke_count , num_LeverPress, Reward_rate, LP_noReward, LP_rate, LA_array, plot, [lat for sublist in bursts for lat in sublist], num_bursts, burst_sums, sum_allburst, avg_allburst, average_burst_press_count] #assigning variables one by one
+        assay_df.loc[df_ind] = [date, Full_ID, g_type, s_type, latency, num_average, Sess_time, progline, num_Rewards, no_headpoke_count , num_LeverPress, Reward_rate, LP_noReward, LP_rate, LA_array, plot, [lat for sublist in bursts for lat in sublist], num_bursts, burst_sums, sum_allburst, avg_allburst, average_burst_press_count] #assigning variables one by one
         df_ind += 1 #location zero is populated with variables above, add one each time for it to be sequential
 
-    # sus_attn_df.to_csv(dirs + csv_name)
-
-sus_attn_df.to_csv(datapath + "burst2-3s_030724.csv")
+assay_df.to_csv(datapath + "burst2-3s.csv")
