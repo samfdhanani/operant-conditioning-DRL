2236024958bfb2036cca79f3ad03b06436bbcf17
Samantha Dhanani <165212066+samfdhanani@users.noreply.github.com>
2024-08-20 20:46:35 -0400

    Add Comments to BurstAnalysis.py

--- BurstAnalysis.py
+++ BurstAnalysis.py
@@ -4,59 +4,59 @@ import numpy as np
 from statistics import mean
 import matplotlib.pyplot as plt
 
-
+# declaring empty list
 filelist = []
 pathslist = []
 
-datapath = os.path.normpath("/users/samdhanani/Desktop/MuhleLab/Operant_Data_Folders/CohortFolder")
-IDList = [1, 2, 3, 4]
+datapath = os.path.normpath("filepath to cohort folder") #add filepath here
+IDList = [1, 2, 3, 4] 
+# list of subject IDs as labelled in MED-PC
 
+# walks through the filepath, filelist contains a list of all files and pathslist contains datapaths for subfolders
 for subdir, dirs, files in sorted(os.walk(datapath)):
     filelist.append(files)
     pathslist.append(subdir)
 
-filelist.pop(0)
-pathslist.pop(0)
+filelist.pop(0) # get rid of elements in index zero
+pathslist.pop(0) # get rid of elements in index zero
 
-def query(pathslist, querydate):  
+# this function is used to isolate and analyze a specific folder/date from the data; run at the botton of this script
+def query(pathslist, querydate): 
     folderdates = []
     for x in pathslist:
-        folderdates.append(os.path.basename(os.path.normpath(x))) #
+        folderdates.append(os.path.basename(os.path.normpath(x))) # extracts the date from the filepath
 
     querypaths = []
 
     for i in range(0, len(folderdates)):
-        if folderdates[i] == querydate:
-            querypaths.append(pathslist[i]) 
+        if folderdates[i] == querydate: # 
+            querypaths.append(pathslist[i]) # pull specific date
 
     return(querypaths)
 
+# this function extracts data from each specific data file by ID 
 def data_pull(datapath, ID):
+    df = []
+    progline = None
     
-    for subdir, dirs, files in sorted(os.walk(datapath)):
+    for subdir, dirs, files in sorted(os.walk(datapath)): # 
         for file in files:
-            temp = file.split('.')  
-            sub = temp[1] 
+            temp = file.split('.') # split string by '.', in this case the string is split into the sesion date (0) and Subject ID (1)
+            sub = temp[1] # Subject ID
             if ID == sub:
-                x = os.path.join(subdir, file) 
-                df = pd.read_csv(x, sep="[:\s]{1,}", skiprows=15, header=None, engine="python") 
-                progline = pd.read_csv(x, skiprows=12, nrows = 1, header = None, engine="python")
+                x = os.path.join(subdir, file) # specific file
+                df = pd.read_csv(x, sep="[:\s]{1,}", skiprows=15, header=None, engine="python") # skips 15 rows to where the data actually starts
+                progline = pd.read_csv(x, skiprows=12, nrows = 1, header = None, engine="python") # reads only 1 row, the program line in the 12th row
                 progline = progline.values.tolist()
-                progline = progline[0][0].split(" ")
+                progline = progline[0][0].split(" ") 
                 if "_" in progline[1]:
-                    progline = progline[1].split("_", 1) 
+                    progline = progline[1].split("_", 1) # splits up program name if an underscore is present
                 progline = progline[1]
-                df = df.drop(0,axis=1)
-                df = df.stack()
-                df = df.to_frame()
-                df = df.to_numpy()
-
-    try:
-        return(df, progline) 
-    except UnboundLocalError:
-        df = []
-        progline = None
-        return(df, progline)   
+                df = df.drop(0,axis=1) # cleaning up the data
+                df = df.stack() 
+                df = df.to_frame() 
+                df = df.to_numpy() # dataframe should be an array of each line containing the data, removed the row labels from the data file 
+    return(df, progline)   
 
 def data_construct(data):
 
@@ -101,39 +101,39 @@ def data_construct(data):
     LeverPress = sorted(LeverPress)
     LeverPress = np.unique(LeverPress)
 
-    headpoke_count = 0
-    within_dipper = False
-    headpoke_occurred = False
+# number of missed headpoke code
+    headpoke_count = 0 # counts the number of headpokes
+    within_dipper = False # tracks if dipper is active
+    headpoke_occurred = False # tracks if headpoke happens
 
     for event in events:
         if event == 25:  # DipOn
-            within_dipper = True
+            within_dipper = True 
         elif event == 26:  # DipOff
             within_dipper = False
             headpoke_occurred = False
 
-        if within_dipper and event == 1011 and not headpoke_occurred:  # HeadPoke
+        if within_dipper and event == 1011 and not headpoke_occurred:
             headpoke_count += 1
             headpoke_occurred = True
 
     no_headpoke_count = len(DipOn) - headpoke_count
-
-    ############################# Latency Calculations:
     
-    
+# calculates lever press rate per second
     LP_rate = np.divide(len(LeverPress), Sess_time)
     LP_rate = LP_rate.tolist()
     x = 0
-
+# calculates lever presses per reward
     Reward_rate = np.divide(len(LeverPress),len(Reward))
     Reward_rate = Reward_rate.tolist()
     x = 0
-
+# calculates the number of lever presses made without a reward
     LP_noReward = np.subtract(len(LeverPress),len(Reward))
     LP_noReward = LP_noReward.tolist()
     x = 0
 
-    LA = []
+# calculating the latency array by finding the time in between lever presses until the last one when LeverPress[i+1] doesn't exist
+    LA = [] # stores individual latencies
 
     for i in range (0, len(LeverPress)):
         p1 = np.divide(LeverPress[i], 10000000)
@@ -142,25 +142,26 @@ def data_construct(data):
         except IndexError:
             break
         else:
-            latency = p2 - p1
-        LA.append(latency)
+            latency = p2 - p1 # next lever press - current lever press
+        LA.append(latency) # add latency to LA
     
         def average_LA(LA_list):
-            total = 0
+            total = 0 # stores sum of all latencies
             for i in LA_list:
-                total += i
-            return total/len(LA_list)
-                
+                total += i # adds latencies values to total
+            return total/len(LA_list) # returns the avg by dividing the total by the number of latencies recorded
+
+# calculates a list of the timestamps of latencies in seconds                
     plot = [latency + sum(LA[:i+1]) for i in range(len(LA))]
 
     LA_array = np.array(LA)
-   
-    bursts = []
-    burst_group = []
+
+    bursts = [] # stores all latency values part of a burst 
+    burst_group = [] # holds latency values part of the same burst
 
     for i in range(len(LA_array)):
     # Check if current value is between 1-2s or less than 1s
-        if LA_array[i] >= 2 and LA_array[i] <= 3:         ######CHANGE 
+        if LA_array[i] >= 1 and LA_array[i] <= 2: # if LA_array[i] <= 1:
             burst_group.append(LA_array[i])
         else:
             if len(burst_group) >= 2:  # ensures that the burst is 2 or more in a row
@@ -170,24 +171,28 @@ def data_construct(data):
     if len(burst_group) >= 2:  # ensures that the burst is 2 or more in a row
         bursts.append(burst_group)
 
+# calculates the sum of each burst group
     burst_sums = [sum(group) for group in bursts]
-
+    
+# calculates the sum of every latency classified as a burst
     sum_allburst = [sum(burst_sums)]
-    sum_allburst = str(sum_allburst)[1:-1]
 
+# finds the average of all burst sums, checks if there is a burst sum and returns an empty cell if there are no bursts found earlier
     if burst_sums:
         avg_allburst = [mean(burst_sums)]
     else:
         avg_allburst = []
 
+# finds the number of presses made in each burst
     burst_press_counts = [len(group) for group in bursts]
 
-    average_burst_press_count = mean(burst_press_counts) if burst_press_counts else 0  # Calculate mean if non-empty
+# finds the average of lever presses made in a burst, returns 0 if there are no burst_press_counts found
+    average_burst_press_count = mean(burst_press_counts) if burst_press_counts else 0 
 
-    avg_allburst = str(avg_allburst)[1:-1] # gets rid of brackets
-
-    burst_sums = str(burst_sums)[1:-1] # gets rid of brackets
-
+# add code to get rid of extra brackets and unecessary punctuation here to allow values to be used in various calculations above
+    burst_sums = str(burst_sums)[1:-1] 
+    sum_allburst = str(sum_allburst)[1:-1] 
+    avg_allburst = str(avg_allburst)[1:-1] 
     LA_array = str(LA_array)[1:-1]
     LA_array = LA_array.replace('\n', ' ')
     LA_array = ' '.join(LA_array.split())
@@ -200,48 +205,62 @@ def data_construct(data):
 
     return(latency, average, Sess_time, len(Reward), no_headpoke_count,len(LeverPress), Reward_rate, LP_noReward, LP_rate, LA_array, plot, bursts, len(bursts), burst_sums, avg_allburst, sum_allburst, average_burst_press_count)
  
+# assigns a label for genotype to the subject
 def genotype(sub):
+    # 1 = WT and 2 = Het
     g_type = None
     if sub == 1 or sub == 2:
-        g_type = 'Control'
+        g_type = 'WT'
     elif sub == 3 or sub == 4:
-        g_type = 'Experimental'
-
+        g_type = 'Het'
     return g_type
 
+# assigns a label for sex to the subject
 def sex(sub):
     s_type = None
     if sub == 1 or sub == 3:
-        s_type = 'Male'
+        s_type = 'M'
     elif sub == 2 or sub == 4:
-        s_type = 'Female'
-
+        s_type = 'F'
     return s_type
 
-df_ind = 0 #index varibale, add one everytime we run through a subj
-assay_df = pd.DataFrame(columns = ['Date', 'Subject', 'Genotype', 'Sex', 'FirstLatency', 'AverageLatency', 'SessionTime', 'Program', 'NumberOfRewards', 'no headpoke count','Lever Press', 'Reward Rate', 'lever press no reward', 'Rate (LP/SessTime)', 'all latency','plot','bursts','number of bursts', 'burst sums','average burst time', 'sum of all bursts', 'average_burst_press_count'])
-def new_func(g_type, ID):
+# uncomment the line below to analyze one specific day in the data, date must match folder name
+# pathslist = query(pathslist, '6-14-22')
+
+df_ind = 0 # index variable, add one everytime we run through a subject
+
+# comment the line below when running the script for one day only 
+burst_df = pd.DataFrame(columns = ['Date', 'Subject', 'Genotype', 'Sex', 'Program', 'First Latency', 'Average Latency', 'Session Time',  'Number Of Rewards', 'Headpokes Missed','Lever Presses', 'Reward Efficiency', 'Lever Presses Made Without a Reward', 'Lever Presses per Second', 'All Latencies','Raster Plot Values','Latencies Defined as a Burst','Number of Bursts', 'Individual Burst Sums','Average Burst Length', 'Sum of all Bursts', 'Avg Presses in a Burst'])
+def new_func(session_type, ID, progline):
+    sess_type = session_type(progline, ID)
+    return sess_type
+def get_genotype(ID): # defines a function using ID and stores the genotype labels in g_type 
     g_type = genotype(ID)
     return g_type
-def new_func(s_type, ID):
+def get_sex(ID): # defines a function using ID and stores the sex labels in s_type
     s_type = sex(ID)
     return s_type
 
 for dirs in pathslist:
 
     date = os.path.basename(os.path.normpath(dirs))
-    csv_name = "\\" + date + ".csv"
+    # uncomment the line below to analyze one specific day in the data
+    # burst_df = pd.DataFrame(columns = ['Date', 'Subject', 'Genotype', 'Sex', 'Program', 'First Latency', 'Average Latency', 'Session Time',  'Number Of Rewards', 'Headpokes Missed','Lever Presses', 'Reward Efficiency', 'Lever Presses Made Without a Reward', 'Lever Presses per Second', 'All Latencies','Raster Plot Values','Latencies Defined as a Burst','Number of Bursts', 'Individual Burst Sums','Average Burst Length', 'Sum of all Bursts', 'Avg Presses in a Burst'])
     
-    for ID in IDList: #run througb ID list one by one, add subj to this to prevent confusion
+    for ID in IDList: # run through ID list one by one, add 'Subject' to this to prevent confusion
         Full_ID = "Subject " + str(ID)
-        data, progline = data_pull(dirs, Full_ID) #calling datapull and putting it through dirs
+        data, progline = data_pull(dirs, Full_ID) # calling datapull and putting it through dirs
         if len(data) == 0:
             continue
         latency, num_average, Sess_time, num_Rewards, no_headpoke_count, num_LeverPress, Reward_rate, LP_noReward, LP_rate, LA_array, plot, bursts, num_bursts, burst_sums, sum_allburst, avg_allburst, average_burst_press_count = data_construct(data) #data construct, putting that all here
         g_type = genotype(ID)
         s_type = sex(ID)
+        # comment the line below to analyze one specific day in the data
+        burst_df.loc[df_ind] = [date, Full_ID, g_type, s_type, progline, latency, num_average, Sess_time, num_Rewards, no_headpoke_count , num_LeverPress, Reward_rate, LP_noReward, LP_rate, LA_array, plot, [lat for sublist in bursts for lat in sublist], num_bursts, burst_sums, sum_allburst, avg_allburst, average_burst_press_count] #assigning variables one by one
+        df_ind += 1 # location zero is populated with variables above, add one each time for it to be sequential
 
-        assay_df.loc[df_ind] = [date, Full_ID, g_type, s_type, latency, num_average, Sess_time, progline, num_Rewards, no_headpoke_count , num_LeverPress, Reward_rate, LP_noReward, LP_rate, LA_array, plot, [lat for sublist in bursts for lat in sublist], num_bursts, burst_sums, sum_allburst, avg_allburst, average_burst_press_count] #assigning variables one by one
-        df_ind += 1 #location zero is populated with variables above, add one each time for it to be sequential
+    # uncomment the line below to analyze one specific day in the data
+    # burst_df.to_csv(dirs + csv_name)
 
-assay_df.to_csv(datapath + "burst2-3s.csv")
+# comment the line below to analyze one specific day in the data
+burst_df.to_csv(datapath + "burst_Xs.csv")
